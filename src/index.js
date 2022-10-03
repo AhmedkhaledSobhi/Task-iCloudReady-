05a7437703c55b27ef9c8be507a7a93240921550
Ahmed khaled <105175448+ahmedKhaleSobhiKhattab@users.noreply.github.com>
2022-10-03 10:44:40 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
        <App />
-    </BrowserRouter>
+    </HashRouter>
    
   </React.StrictMode>
 );
